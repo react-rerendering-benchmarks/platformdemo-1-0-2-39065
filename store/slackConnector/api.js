b0f29b25a86d49280092c041d36512bbdd028e7c
Crowdbotics <engineering@crowdbotics.com>
2023-02-22 21:46:06 +0000

    Update store.

--- store/slackConnector/api.js
+++ store/slackConnector/api.js
@@ -1,16 +1,12 @@
-import axios from "axios";
+import axios from "axios"
 const slackConnector = axios.create({
   baseURL: "https://cb-sales-demos-ent--38169.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function slackconnector_get__read(payload) {
-  return slackConnector.post(`/modules/slack/service/create-channel/`, payload.data);
+  return slackConnector.post(
+    `/modules/slack/service/create-channel/`,
+    payload.data
+  )
 }
-
-export const apiService = {
-  slackconnector_get__read
-};
+export const apiService = { slackconnector_get__read }
--- store/slackConnector/slackconnector_response_get_Newdatacalls.slice.js
+++ store/slackConnector/slackconnector_response_get_Newdatacalls.slice.js
@@ -1,16 +1,13 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "./api";
-export const slackconnector_get__read = createAsyncThunk("slackconnector_response_get_Newdatacalls/slackconnector_get__read", async payload => {
-  const response = await apiService.slackconnector_get__read(payload);
-  return response.data;
-});
-const initialState = {
-  entities: [],
-  api: {
-    loading: "idle",
-    error: null
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const slackconnector_get__read = createAsyncThunk(
+  "slackconnector_response_get_Newdatacalls/slackconnector_get__read",
+  async payload => {
+    const response = await apiService.slackconnector_get__read(payload)
+    return response.data
   }
-};
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
 const slackconnector_response_get_NewdatacallsSlice = createSlice({
   name: "slackconnector_response_get_Newdatacalls",
   initialState,
@@ -18,24 +15,24 @@ const slackconnector_response_get_NewdatacallsSlice = createSlice({
   extraReducers: {
     [slackconnector_get__read.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending";
+        state.api.loading = "pending"
       }
     },
     [slackconnector_get__read.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload);
-        state.api.loading = "idle";
+        state.entities.push(action.payload)
+        state.api.loading = "idle"
       }
     },
     [slackconnector_get__read.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
+        state.api.error = action.error
+        state.api.loading = "idle"
       }
     }
   }
-});
+})
 export default {
   slackconnector_get__read,
   slice: slackconnector_response_get_NewdatacallsSlice
-};
+}
