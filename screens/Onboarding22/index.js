2f4b70076df4e79e16eeb2982811b356291e004f
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-02-22 22:51:14 +0100

    Crowdbotics Studio: Update app screens

--- screens/Onboarding22/index.js
+++ screens/Onboarding22/index.js
@@ -1,13 +1,8 @@
 import React from "react";
-import {
-  Text,
-  View,
-  StyleSheet, ScrollView, Image, Pressable
-} from "react-native";
+import { Text, View, StyleSheet, ScrollView, Image, Pressable } from "react-native";
 
 const Onboarding2 = () => {
-  return (
-    <ScrollView style={styles.container}>
+  return <ScrollView style={styles.container}>
       <View style={styles.header}>
         <Image source={require("./assets/back.png")} style={styles.back} />
         <Image source={require("./assets/slide.png")} style={styles.slide} />
@@ -23,9 +18,9 @@ const Onboarding2 = () => {
         </Text>
       </View>
       <Button buttonText={"Get started"} />
-    </ScrollView>
-  );
+    </ScrollView>;
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -39,15 +34,40 @@ const styles = StyleSheet.create({
     marginTop: 15,
     marginBottom: 30
   },
-  back: { width: 11.25, height: 20, resizeMode: "contain", marginLeft: -15 },
-  slide: { width: 96, height: 8, resizeMode: "contain" },
-  title: { fontSize: 30, marginHorizontal: 25, marginTop: 10, marginBottom: 10 },
-  descContainer: { paddingHorizontal: 25 },
-  desc: { color: "#757575", marginBottom: 5 },
-  imgContainer: { height: 460, width: 343, backgroundColor: "#E5E5EA", borderRadius: 10, alignSelf: "center" }
+  back: {
+    width: 11.25,
+    height: 20,
+    resizeMode: "contain",
+    marginLeft: -15
+  },
+  slide: {
+    width: 96,
+    height: 8,
+    resizeMode: "contain"
+  },
+  title: {
+    fontSize: 30,
+    marginHorizontal: 25,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  descContainer: {
+    paddingHorizontal: 25
+  },
+  desc: {
+    color: "#757575",
+    marginBottom: 5
+  },
+  imgContainer: {
+    height: 460,
+    width: 343,
+    backgroundColor: "#E5E5EA",
+    borderRadius: 10,
+    alignSelf: "center"
+  }
 });
 
-const Button = (params) => {
+const Button = params => {
   const btnStyle = {
     backgroundColor: params.outline ? "#fff" : "#000",
     borderColor: params.outline ? "#000" : "#fff",
@@ -56,14 +76,12 @@ const Button = (params) => {
   const btnText = {
     color: params.outline ? "#000" : "#fff"
   };
-  return (
-    <View style={buttonStyles.btnContainer}>
+  return <View style={buttonStyles.btnContainer}>
       <Pressable style={[buttonStyles.btn, btnStyle]} onPress={params.onPress}>
         <Text style={[buttonStyles.btnText, btnText]}>{params.buttonText}</Text>
         <View style={styles.childrenContainer}>{params.children}</View>
       </Pressable>
-    </View>
-  );
+    </View>;
 };
 
 const buttonStyles = StyleSheet.create({
@@ -96,5 +114,4 @@ const buttonStyles = StyleSheet.create({
     alignItems: "center"
   }
 });
-
 export default Onboarding2;
--- store/signup/api.js
+++ store/signup/api.js
@@ -1,9 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 const signup = axios.create({
   baseURL: "https://tstcr2020092101-20512.botics.co/",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function signup_post_api_v1_signup_read(payload) {
-  return signup.post(`/api/v1/signup`, payload.data)
+  return signup.post(`/api/v1/signup`, payload.data);
 }
-export const apiService = { signup_post_api_v1_signup_read }
+
+export const apiService = {
+  signup_post_api_v1_signup_read
+};
